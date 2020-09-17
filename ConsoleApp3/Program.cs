441035f3fc3a5a402aa2e2d5da7731edccececf5
Andreas Saarenpää <andreas.saarenpaa2@gmail.com>
2020-09-17 11:48:15 +0200

    ConsoleApp3

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Models;
+using SharedLibraries.Services;
 
 namespace ConsoleApp3
 {
     class Program
     {
+
+
+        private static readonly string _conn = "";
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn,TransportType.Mqtt);
         static void Main(string[] args)
         {
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+
             
         }
     }
---
+++ SharedLibraries/Models/TemperaturModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLibraries.Models
+{
+    public class TemperaturModel
+    {
+
+        public double Temperatur { get; set; }
+        public double Humidity { get; set; }
+    }
+
+
+
+}
